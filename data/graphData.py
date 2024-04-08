79007f95464c272d4a0e0a724395642f94097d9f
Mya Schroder <80082701+schromya@users.noreply.github.com>
2024-04-07 18:10:44 -0800

    Added code for displaying any number of csv plots

--- data/graphData.py
+++ data/graphData.py
@@ -1,25 +1,33 @@
 """
+Loops through every csv in the pidCSV folder and graphs them.
 Install pandas and matplotlip. Run this file in this directory with `python3 graphData.py`
 """
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
 
-# Load the CSV file
-csv = "PID_P0.5_I0.0_D0.0.csv"
-df = pd.read_csv(csv)
 
-# plotting a line graph 
+folder_path = 'pidCSV'
 
-plt.subplot(1, 2, 1)
-plt.plot(df["msecElapsed"], df["Control"]) 
-plt.xlabel('Time Elapsed (msec)')
-plt.ylabel('Control Value: Drivetrain Voltage (V)')
+# Iterate over each entry in the folder path and plot each csv
+for i, csv in enumerate(os.listdir(folder_path)):
 
-plt.subplot(1, 2, 2)
-plt.plot(df["msecElapsed"], df["Process"]) 
-plt.plot(df["msecElapsed"], df["Setpoint"], label="Setpoint",  linestyle='--') 
-plt.xlabel('Time Elapsed (msec)')
-plt.ylabel('Process Value: Distance to Position (inch)')
-plt.legend()
+    df = pd.read_csv(f"{folder_path}/{csv}")
+
+    plt.figure(i)
+    plt.title(csv)
+
+    plt.subplot(1, 2, 1)
+    plt.plot(df["msecElapsed"], df["Control"]) 
+    plt.xlabel('Time Elapsed (msec)')
+    plt.ylabel('Control Value: Drivetrain Voltage (V)')
+
+    plt.subplot(1, 2, 2)
+    plt.plot(df["msecElapsed"], df["Process"]) 
+    plt.plot(df["msecElapsed"], df["Setpoint"], label="Setpoint",  linestyle='--') 
+    plt.xlabel('Time Elapsed (msec)')
+    plt.ylabel('Process Value: Distance to Position (inch)')
+    plt.legend()
+    
 plt.show() 
